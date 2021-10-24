ce1e0d3219abbf66d9fbf2f38c9fd4cfc98bb095
Igor Gomes <igorgomesads@gmail.com>
2021-10-24 20:19:07 -0300

    feat: melhora e recursos
    modificado index.html para index.php, usando recursos do bootstrap, mudança na localização das imagens de /css/imagens para /images, acrescentado mais estilos

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <nav class="menu">
+    <ul>
+      <img src="images/GEBCA_LOGO.png" alt="GEBCA" width="60" height="60">
+      <li><a class="active" href="#">Início</a></li>
+      <li><a href="#">Participar</a></li>
+      <li><a href="#">Contatos</a></li>
+      <li><a href="#">Quem somos?</a></li>
+    </ul>
+  </nav>
+
+  <!-- carrocel de imagens -->
+  <div id="carouselBanner" class="carousel slide mb-2" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/e1.jpg" class="d-block w-100" alt="imagem ilustrativa">
+      </div>
+      <div class="carousel-item">
+        <img src="images/e2.jpg" class="d-block w-100" alt="imagem ilustrativa">
+      </div>
+      <div class="carousel-item">
+        <img src="images/e3.jpg" class="d-block w-100" alt="imagem ilustrativa">
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <!-- evento do grupo escoteiro -->
+    <div class="card text-center">
+      <div class="card-header">
+        Atividade
+      </div>
+      <div class="card-body">
+        <img src="images/jota-joti-logo.png" width="100" alt="logo do jota-joti">
+        <h5 class="card-title">JOTA-JOTI 2021</h5>
+        <p class="card-text">O Jamboree no Ar e o Jamboree na Internete espera por você.</p>
+        <a href="#" class="btn btn-primary">Venha partitipar!</a>
+      </div>
+      <div class="card-footer text-muted">
+        15 a 17 de Agosto 2021
+      </div>
+    </div>
+
+    <!-- uma citação -->
+    <blockquote class="blockquote bg-warning m-2">
+      <p class="mb-0">Deixe aqui uma citação importante ou uma mensagem.</p>
+      <footer class="blockquote-footer">fica muito interessante aos visitantes<cite title="Source Title">Igor Gomes 13º/Ce.</cite></footer>
+    </blockquote>
+  </div>
+
+<footer class="footer p-5">
+  <p>Endereço:</p>
+  <p>
+    Contato <br>
+    Telefone: (85) 90000-0000<br>
+    e-Mail: seue-mail@email.com
+  </p>
+</footer >
+  <!-- Scripts -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+
+</html>
